8e46b5bba1aa9c68b074ec621cab6094a40aa520
Fabi <reichmuth.fabian@gmail.com>
2024-11-28 15:22:21 +0100

    allow tooltip tag and tooltip attributes

--- django_prose_editor/sanitized.py
+++ django_prose_editor/sanitized.py
@@ -1,10 +1,27 @@
+from copy import deepcopy
 from django_prose_editor.fields import ProseEditorField, _actually_empty
+import nh3
+
+
+_allowed_attributes = deepcopy(nh3.ALLOWED_ATTRIBUTES)
+_allowed_attributes["*"] = {
+    "data-tooltip-text",
+    "data-placement",
+}
+_allowed_tags = nh3.ALLOWED_TAGS | {
+    "tooltip",
+}
 
 
 def _nh3_sanitizer():
-    from nh3 import clean
 
-    return lambda x: _actually_empty(clean(x))
+    return lambda x: _actually_empty(
+        nh3.clean(
+            x,
+            tags=_allowed_tags,
+            attributes=_allowed_attributes,
+        )
+    )
 
 
 class SanitizedProseEditorField(ProseEditorField):
